39c32df1897cdc884103bb78f302688a3024d66a
Roopaksh <72032743+Roopaksh1@users.noreply.github.com>
2022-10-05 18:48:45 +0530

    Add footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,11 @@
+import "../styles/footer.css";
+
 const footer = () => {
-  
-}
+  const footer = document.createElement("div");
+  footer.classList.add("footer");
+  footer.textContent = "Created by RNS"
+
+  return footer;
+};
 
 export default footer;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import "./styles/global.css";
 import header from "./components/header";
+import footer from "./components/footer";
 
 const content = document.querySelector("#content");
 content.appendChild(header());
+content.appendChild(footer());
