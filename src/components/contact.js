d4f0ff032f2a7eb403f980cc3e3219f82e1f5409
Roopaksh <72032743+Roopaksh1@users.noreply.github.com>
2022-10-06 20:27:55 +0530

    Update contact

--- src/components/contact.js
+++ src/components/contact.js
@@ -13,10 +13,9 @@ const contact = () => {
   const p1 = document.createElement("p");
   const p2 = document.createElement("p");
   p1.innerHTML = `<img src=${phoneImg} width=30> 987 654 321`;
-  p2.innerHTML = `<img src=${locationImg} width= 30> McDonald's`;
+  p2.innerHTML = `<img src=${locationImg} width= 30> Restaurant`;
   const map = document.createElement("div");
-  map.innerHTML =
-    `<div class="mapouter"><div class="gmap_canvas"><iframe class="gmap_iframe" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=McDonald's, Plot No 2, B Block, Community Center, Block BD, West Shalimar Bagh, Shalimar Bagh, New Delhi, Delhi 110088&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe><a href="https://mcpenation.com/">MCPE Nation</a></div><style>.mapouter{position:relative;text-align:right;width:600px;height:400px;}.gmap_canvas {overflow:hidden;background:none!important;width:600px;height:400px;}.gmap_iframe {width:600px!important;height:400px!important;}</style></div>`;
+  map.innerHTML = `<div class="mapouter"><div class="gmap_canvas"><iframe class="gmap_iframe" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe><a href="https://mcpenation.com/">MCPE Nation</a></div><style>.mapouter{position:relative;text-align:right;width:600px;height:400px;}.gmap_canvas {overflow:hidden;background:none!important;width:600px;height:400px;}.gmap_iframe {width:600px!important;height:400px!important;}</style></div>`;
   contact.append(h1, p1, p2, map);
 
   return contact;
